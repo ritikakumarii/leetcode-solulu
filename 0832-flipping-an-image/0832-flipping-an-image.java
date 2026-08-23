a5690ea73c7e79ab32d90a30e4a0e24802bdcb11
ritikakumarii <ritikagupta8271@gmail.com>
2026-08-23 23:29:08 +0530

    Time: 0 ms (100%), Space: 45.4 MB (97.16%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -3,22 +3,31 @@ class Solution {
         for(int row =0 ; row< image.length ; row++){
             int i =  0;
             int j = image[0].length-1;
-            while(i<j){
+            // while(i<j){
+            //     int temp = image[row][i];
+            //     image[row][i]=image[row][j];
+            //     image[row][j]= temp;
+            //     i++;
+            //     j--;
+            // } // no need for alag se loop chalan to convert the values
+            // for(int k=0; k<image.length ; k++){
+            //     if (image[row][k]==1){
+            //         image[row][k]=0;
+            //     }
+            //     else{
+            //         image[row][k]=1;
+            //     }
+
+            //simply invert the values while swapping
+            while(i<=j){
                 int temp = image[row][i];
-                image[row][i]=image[row][j];
-                image[row][j]= temp;
+                image[row][i]=1-image[row][j];
+                image[row][j]=1-temp;
                 i++;
                 j--;
             }
-            for(int k=0; k<image.length ; k++){
-                if (image[row][k]==1){
-                    image[row][k]=0;
-                }
-                else{
-                    image[row][k]=1;
-                }
             }
-        }
+        
         return image;
     }
 }
