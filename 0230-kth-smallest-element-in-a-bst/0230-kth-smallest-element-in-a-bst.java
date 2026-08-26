39e7aeff934a96827367c493627eebd1e6f9c1e2
ritikakumarii <ritikagupta8271@gmail.com>
2026-08-27 00:06:14 +0530

    Time: 0 ms (100%), Space: 46.9 MB (17.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,17 +14,28 @@
  * }
  */
 class Solution {
-    public List<Integer> inOrder(TreeNode root,List<Integer> list){
+    // public List<Integer> inOrder(TreeNode root,List<Integer> list){
         
-        if(root==null) return list ;
-        inOrder(root.left,list);
-        list.add(root.val);
-        inOrder(root.right,list);
-        return list;
+    //     if(root==null) return list ;
+    //     inOrder(root.left,list);
+    //     list.add(root.val);
+    //     inOrder(root.right,list);
+    //     return list;
+    // }
+    int count=0;
+    public TreeNode fun(TreeNode root, int k) {
+    //   List<Integer> list = new ArrayList<>();
+    //   list=inOrder(root,list);
+    //   return list.get(k-1);
+    if(root==null) return root;
+    TreeNode left = fun(root.left,k);
+    if(left!=null) return left;
+    count++;
+    if(count==k) return root;
+    return fun(root.right,k);
     }
-    public int kthSmallest(TreeNode root, int k) {
-      List<Integer> list = new ArrayList<>();
-      list=inOrder(root,list);
-      return list.get(k-1);
+    public int kthSmallest(TreeNode root, int k){
+    TreeNode ans = fun(root,k);
+    return ans.val;
     }
 }
