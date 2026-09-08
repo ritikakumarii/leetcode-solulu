079576ea9dcae4d98ef85e862087f3368bd53051
ritikakumarii <ritikagupta8271@gmail.com>
2026-09-08 23:12:48 +0530

    Time: 1 ms (88.53%), Space: 43.2 MB (61.1%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,31 +1,35 @@
 class Solution {
-    public boolean all(String word){
-        for(int i =0 ; i< word.length() ; i++){
-                char ch = word.charAt(i);
+    // public boolean all(String word){
+    //     for(int i =0 ; i< word.length() ; i++){
+    //             char ch = word.charAt(i);
         
-            if(!Character.isUpperCase(ch)) return false;
-        }
-        return true;
-    }
-    public boolean none(String word){
-        for(int i =0 ; i< word.length() ; i++){
-                char ch = word.charAt(i);
-            if(!Character.isLowerCase(ch)) return false;
-        }
-        return true;
-    }
-    public boolean onlyFirst(String word){
-        if(Character.isUpperCase(word.charAt(0))){
-            for(int i =1 ; i< word.length() ; i++){
-                char ch = word.charAt(i);
-             if(!Character.isLowerCase(ch)) return false;
-            }
+    //         if(!Character.isUpperCase(ch)) return false;
+    //     }
+    //     return true;
+    // }
+    // public boolean none(String word){
+    //     for(int i =0 ; i< word.length() ; i++){
+    //             char ch = word.charAt(i);
+    //         if(!Character.isLowerCase(ch)) return false;
+    //     }
+    //     return true;
+    // }
+    // public boolean onlyFirst(String word){
+    //     if(Character.isUpperCase(word.charAt(0))){
+    //         for(int i =1 ; i< word.length() ; i++){
+    //             char ch = word.charAt(i);
+    //          if(!Character.isLowerCase(ch)) return false;
+    //         }
             
-        }
-        else return false;
-        return true;
-    }
+    //     }
+    //     else return false;
+    //     return true;
+    // }
     public boolean detectCapitalUse(String word) {
-      return (all(word) || none(word) )|| onlyFirst(word);
+        int cnt =0 ;
+        for(int i =0 ; i< word.length() ; i++){
+            if(Character.isUpperCase(word.charAt(i))) cnt++;
+        }
+      return (cnt==0 || cnt==word.length() )|| (cnt==1 && Character.isUpperCase(word.charAt(0))) ;
     }
 }
